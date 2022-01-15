ee3e08d1da406f080402ec1965297a60feadf95c
RohanTapulli1311 <74230154+RohanTapulli1311@users.noreply.github.com>
2022-01-15 13:56:14 +0530

    updated blackjack files

--- secondProject/blackJack.js
+++ secondProject/blackJack.js
@@ -1,20 +1,30 @@
-let firstCard = 4
-let secondCard = 9
+let firstCard = 10
+let secondCard = 11
 let hasBlackJack = false
-let sum = firstCard + secondCard + 4
+let sum = firstCard + secondCard 
 let isAlive = true
-if (sum <= 21){
-    console.log("Would you like to draw another card?")
+let message = ""
+let messageEl = document.getElementById('message-el')
+function startGame(){
+    if (sum <= 20){
+        message = "Would you like to draw another card?" 
+        
+    }
+    else if (sum === 21){
+        message = "Congrats!! Youve got blackJack!!"
+        
+        hasBlackJack = true
+    // make sure u use === alwayss
+    }
+    else{
+        message = "Sry, you are out!!"
+        
+        isAlive = false
+    }
+messageEl.textContent = message
+console.log(message)
 }
-else if (sum === 21){
-    console.log("Congrats!! Youve got blackJack!!")
-    hasBlackJack = true
-// make sure u use === alwayss
-}
-else{
-    console.log("Sry, you are out!!") 
-    isAlive = false
-}
+
 
 console.log(hasBlackJack)
 console.log(isAlive)
