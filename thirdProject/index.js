af4b411f72f4ed9b2f71373525e23a0f0a57431a
RohanTapulli1311 <74230154+RohanTapulli1311@users.noreply.github.com>
2022-01-20 16:30:01 +0530

    Added localStorage feature for the chrome extension project

--- thirdProject/index.js
+++ thirdProject/index.js
@@ -1,15 +1,52 @@
 let myLeads =[]
+ 
+// myLeads = JSON.parse(myLeads)
+// console.log(typeof myLeads)
+// myLeads.push("www.awesomeleads.com")
+// console.log(myLeads)
+// myLeads = JSON.stringify(myLeads)
+// console.log(typeof myLeads)
+
 //Always if possible use const, if not then let
 // we are using const cuz we dont want to change the input text
+
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
+const ulEl = document.getElementById('ul-el')
+const delBtn = document.getElementById('delete-btn')
+//both key and value in localStorage should be strings
+// thus we JSON.stringify() and JSON.parse() methods to pass array into localStorage
+
+//localStorage.setItem("myLeads", "www.linkedin.com")
+//console.log(localStorage.getItem("myLeads"))
+//localStorage.clear()
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
 inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
-    inputEl.value = ''
+    if(inputEl.value!=""){
+        myLeads.push(inputEl.value)
+        inputEl.value = ''
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        renderLeads()
+        console.log(localStorage.getItem("myLeads"))
+    }
+    
+})
+
+delBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
     renderLeads()
 })
 
-const ulEl = document.getElementById('ul-el')
+
+
+
 
 function renderLeads(){
     let listItems =""
