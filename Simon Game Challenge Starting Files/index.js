67939e676f38681fb5461ea27fbc682fa7e945b1
RohanTapulli1311 <74230154+RohanTapulli1311@users.noreply.github.com>
2022-02-03 12:12:19 +0530

    completed project

--- Simon Game Challenge Starting Files/index.js
+++ Simon Game Challenge Starting Files/index.js
@@ -1,134 +1,91 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+let userClickedPattern = []
 let level = 0
-let patternGenerated = []
-let patternEntered = []
+let started = false
 
-const blocks = ['green','red','yellow','blue']
-
-$(document).on("keypress",startGame)
-
-function startGame(){
-   level = 1
-   startLevel()
-   patternGenerated = []
-   patternEntered = []
-}
-
-function startLevel(){
-    $("#level-title").text(`level ${level}`)
-    let randomChoice = randomPattern()
-    // console.log(randomChoice)
-    // console.log(blocks[randomChoice-1])
-    randomPlay(blocks[randomChoice-1])
-    patternGenerated.push(blocks[randomChoice-1])
-    console.log(patternGenerated)
-    $('.btn').on("click", function(){
-        //alert(this.id)
-        pressedChoice(this.id)
-        patternEntered.push(this.id)
-        console.log(patternEntered)
-        checkCorrect()
-    })
-
-}
-function checkCorrect(){
-    for(let i=0;i<patternGenerated.length;i++){
-        if(patternGenerated[i] === patternEntered[i]){
-            continue
-        }
-        else{
-            endGame()
-        }
-      
+$(document).on("keypress",function(){
+    if(started === false){
+        nextSequence()
+        started = true
     }
-    alert("next round")
-    startLevel()
-}
-function endGame(){
- alert("game ended")
- startGame()
-}
-
-function randomPattern(){
-   return (Math.floor(Math.random()*4)+1)
-}
-
-function randomPlay(choice){
-   switch(choice){
-       case 'green':
-            let greenPlay = new Audio('sounds/green.mp3');
-            $('.green').toggle("pressed")
-            setTimeout(function(){
-                $('.green').toggle("pressed") 
-            },100)
-            greenPlay.play();
-            break;
-       case 'red':
-        let redPlay = new Audio('sounds/red.mp3');
-        $('.red').toggle("pressed")
-        setTimeout(function(){
-            $('.red').toggle("pressed") 
-        },100)
-        redPlay.play();
-        break;
-        case 'yellow':
-            let yellowPlay = new Audio('sounds/yellow.mp3');
-            $('.yellow').toggle("pressed")
-            setTimeout(function(){
-                $('.yellow').toggle("pressed") 
-            },100)
-            yellowPlay.play();
-            break;
-        case 'blue':
-            let bluePlay = new Audio('sounds/blue.mp3');
-            $('.blue').toggle("pressed")
-            setTimeout(function(){
-                $('.blue').toggle("pressed") 
-            },100)
-            bluePlay.play();
-            break;
-        default:
-            alert("Something went wrong in switch")
+})
 
 
-   }
-}
-function pressedChoice(choice){
-    switch(choice){
-        case 'green':
-             let greenPlay = new Audio('sounds/green.mp3');
-             $('.green').addClass("pressed")
-             setTimeout(function(){
-                 $('.green').removeClass("pressed") 
-             },100)
-             greenPlay.play();
-             break;
-        case 'red':
-         let redPlay = new Audio('sounds/red.mp3');
-         $('.red').addClass("pressed")
-         setTimeout(function(){
-             $('.red').removeClass("pressed") 
-         },100)
-         redPlay.play();
-         break;
-         case 'yellow':
-             let yellowPlay = new Audio('sounds/yellow.mp3');
-             $('.yellow').addClass("pressed")
-             setTimeout(function(){
-                 $('.yellow').removeClass("pressed") 
-             },100)
-             yellowPlay.play();
-             break;
-         case 'blue':
-             let bluePlay = new Audio('sounds/blue.mp3');
-             $('.blue').addClass("pressed")
-             setTimeout(function(){
-                 $('.blue').removeClass("pressed") 
-             },100)
-             bluePlay.play();
-             break;
-         default:
-             alert("Something went wrong in switch")
- 
- 
+$('.btn').on("click",function(){
+
+let userChosenColour = this.id
+userClickedPattern.push(userChosenColour)
+currentClick = userChosenColour
+console.log(userClickedPattern)
+playSound(userChosenColour)
+animatePress(userChosenColour)
+checkAnswer(userClickedPattern.length - 1)
+
+})
+            
+       
+
+
+
+function nextSequence() {
+    userClickedPattern = []
+    level += 1
+    $('#level-title').text("level " + level)
+
+    //2. Inside the new function generate a new random number between 0 and 3, and store it in a variable called randomNumber
+    var randomNumber = Math.floor(Math.random() * 4);
+  
+    //4. Create a new variable called randomChosenColour and use the randomNumber from step 2 to select a random colour from the buttonColours array.
+    var randomChosenColour = buttonColours[randomNumber];
+  
+    //6. Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
+    gamePattern.push(randomChosenColour);
+
+    $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour)
+    
+
+  }
+
+  function playSound(name){
+    let playAudio = new Audio(`sounds/${name}.mp3`);
+    playAudio.play();
+  }
+  
+  function animatePress(currentColour){
+    $(`#${currentColour}`).addClass("pressed")
+    setTimeout(function(){
+        $(`#${currentColour}`).removeClass("pressed") 
+    },100)
+  }
+
+  function checkAnswer(currenLevel){
+      if(gamePattern[currenLevel] === userClickedPattern[currenLevel]){
+          console.log("correct")
+
+          if(gamePattern.length === userClickedPattern.length){
+              setTimeout(function(){
+              nextSequence()    
+              }, 1000)
+          }
+      }
+      else{
+          console.log("incorrect")
+          playSound("wrong")
+          $("body").addClass("game-over")
+          setTimeout(function(){
+            $("body").removeClass("game-over")
+          },200)
+          $('#level-title').text("Game Over, Press Any Key to Restart")
+          startOver()
+
+
+      }
+  }
+
+  function startOver() {
+    gamePattern =[]
+    level = 0
+    started = false
     }
- }
